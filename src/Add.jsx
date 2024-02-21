3a3d899e42625c353ac2ed3de2ec5cd46abf3577
melekladh <ladharimalek10@gmail.com>
2024-02-21 09:34:21 +0100

    first commit

---
+++ src/Add.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react'
+import StarRatingComponent from 'react-star-rating-component';
+
+const Add = (props) => {
+    const title =useRef()
+    const description =useRef()
+    const rating =useRef()
+    const posterURL =useRef()
+const [rate,setRate]=useState(0)
+
+  return (
+    <div>
+        <input ref={title} type="text" />
+        <input ref={description} type="text" />
+        <input ref={rating} type="text" />
+        <input ref={posterURL} type="text" />
+        <StarRatingComponent 
+          name="rate1" 
+          starCount={10}
+          value={rate}
+          onStarClick={(value)=>{setRate (value)}}
+        />
+
+<button onClick={()=>{props.setMovies([...props.movies , {title:title.current.value,
+description:description.current.value,
+rating:rating.current.value,
+posterURL:posterURL.current.value}])}}>add</button>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/Filter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Filter = (props) => {
+
+  return (
+
+    <div>
+      <input  type="text" placeholder='search by title' onChange={(event)=>props.settitleSearch(event.target.value)} />
+
+    </div>
+  )
+}
+
+export default Filter
